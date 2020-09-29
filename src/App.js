9061f5f236ed3e63d1721cfaada63fcf3e4425c5
anuj-10 <anuj10jaiswal@gmail.com>
2020-09-29 17:03:04 +0530

    code refractoring and hash url generated for video

--- src/App.js
+++ src/App.js
@@ -1,87 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { fetchAllCategories, fetchCategoryData } from "./api";
+import React from "react";
 import "./App.css";
-import Cards from "./components/Cards";
-import Navigation from "./components/Navigation";
-import { useNavigation } from "./hooks/useNavigation";
-import Search from "./components/Search";
-
+import MainComponent from "./components/MainComponent";
 function App() {
-  const [category, setCategory] = useState([]);
-  const [cardData, setCardData] = useState([]);
-  const [filteredCardData, setFilteredCardData] = useState([]);
-  const [currentNavIndex, setCurrentNavIndex] = useState(0);
-  const [currentCardIndex, setCurrentCardIndex] = useState(-1);
-  const [isNavActive, setIsNavActive] = useState(true);
-  const [search, setSearch] = useState("");
-
-  const [current, setNavigation] = useNavigation({
-    onUpKey: () => setCurrentCardIndex((i) => (i > -1 ? i - 1 : i)),
-    onDownKey: (len) => {
-      setCurrentCardIndex((i) => (len > i ? i + 1 : i));
-      setIsNavActive(false);
-    },
-    onLeftKey: (len) => setCurrentNavIndex((i) => (i === 0 ? len - 1 : i - 1)),
-    onRightKey: (len) =>
-      setCurrentNavIndex((i) => (i + 1 > len - 1 ? 0 : i + 1)),
-  });
-
-  useEffect(() => {
-    async function fetchData() {
-      const categories = await fetchAllCategories();
-      setCategory(categories);
-      const firstCategoryData = await fetchCategoryData(categories[0].link);
-      setCardData(firstCategoryData);
-      setFilteredCardData(firstCategoryData);
-      current.allCategoriesLength = categories.length;
-      current.cardLength = firstCategoryData.length;
-      setNavigation(current);
-    }
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    if (currentCardIndex === -1) {
-      setIsNavActive(true);
-      current.navigationType = "nav";
-      setNavigation(current);
-    }
-  }, [currentCardIndex]);
-
-  useEffect(() => {
-    async function fetchData() {
-      const categoryData = await fetchCategoryData(
-        category[currentNavIndex].link
-      );
-      setCardData(categoryData);
-      setFilteredCardData(categoryData);
-      current.cardLength = categoryData.length;
-      setNavigation(current);
-    }
-    current.navigationType === "nav" &&
-      category[currentNavIndex] &&
-      fetchData();
-  }, [currentNavIndex]);
-
-  const onSearch = (search) => {
-    const filteredArray = cardData.filter((i) => {
-      return i.title.toLowerCase().match(search);
-    });
-    setFilteredCardData(filteredArray);
-  };
-
   return (
     <div className="App">
-      <Search onSearch={onSearch} />
-      <Navigation
-        category={category}
-        currentNavIndex={currentNavIndex}
-        isNavActive={isNavActive}
-      />
-      <Cards
-        categoryData={filteredCardData}
-        currentCardIndex={currentCardIndex}
-      />
+      <MainComponent />
     </div>
   );
 }
--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,9 @@
 import axios from "axios";
+import md5 from "md5";
 
 const url = `http://prod.api.indiascience.in/catalog_lists/`;
+const video_parameters = "?service_id=6&play_url=yes&protocol=hls&us=";
+const hash_string = "ywVXaTzycwZ8agEs3ujx";
 
 export const fetchAllCategories = async () => {
   let changeableUrl = `${url}/all-categories-list?region=IN&auth_token=Ts4XpMvGsB2SW7NZsWc3`;
@@ -29,10 +32,13 @@ export const fetchCategoryData = async (categoryName) => {
       `${url}/${categoryName}?region=IN&auth_token=Ts4XpMvGsB2SW7NZsWc3`
     );
     return catalog_list_items.map((value, index) => {
+      const url = value.smart_url + video_parameters;
+      const hash = md5(hash_string + url);
       return {
         title: value.display_title,
         image: value.thumbnails,
         id: index,
+        url: `${url}${hash}`,
       };
     });
   } catch (error) {
